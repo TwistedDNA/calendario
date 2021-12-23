8de96d9ba694e7de82e12431bb744e70ee81e71b
TwistedDNA <legols93@gmail.com>
2021-12-23 17:27:37 +0100

    removed Healthcheck controller, as it was a recruitment task, and not supposed to stay here

--- src/test/java/learn/by/practice/calendario/timeslot/TimeslotTestConfiguration.java
+++
@@ -1,9 +0,0 @@
-package learn.by.practice.calendario.timeslot;
-
-import org.springframework.context.annotation.ComponentScan;
-import org.springframework.context.annotation.Configuration;
-
-@Configuration
-@ComponentScan
-public class TimeslotTestConfiguration {
-}
